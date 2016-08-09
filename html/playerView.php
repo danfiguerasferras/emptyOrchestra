6643963200ce0bd43f1502b466dd195cd80d8354
blad3r <blad3dancer@gmail.com>
2016-08-09 20:02:35 +0200

    The lyrics follows the player time. Starting to create the lyrics

--- html/playerView.php
+++ html/playerView.php
@@ -7,11 +7,29 @@
  */
 ?>
 <script src="../js/player.js"></script>
+<link rel="stylesheet" type="text/css" href="../css/player.css">
 
 <body>
-    <audio controls id="audioPlayer" onplay="hello();">
-        <source src="../src/001_en_peu_de_guerra.mp3" type="audio/mpeg"/>
+    <audio controls id="audioPlayer" onplay="startLyricTrack();" onpause="stopLyricTrack();">
+        <source src="../src/music/001_en_peu_de_guerra.mp3" type="audio/mpeg"/>
         Your browser does not support the audio tag.
     </audio>
     <button name="test" onclick="javascript:getSecondPlayer();">Test</button>
+    <div id="catalanLyrics">
+        <p class="lyricsLanguageTitle">Language: Catalan</p>
+        <div class="lyricsContainer">
+            <p id="cat_line_1" class="lyricsLine"></p>
+            <p id="cat_line_2" class="lyricsLine"></p>
+            <p id="cat_line_3" class="actualLyricsLine lyricsLine"></p>
+            <p id="cat_line_4" class="lyricsLine"></p>
+            <p id="cat_line_5" class="lyricsLine"></p>
+        </div>
+    </div>
+
+    <button name="test2" onclick="javascript:previousLine();">Test -></button>
+    <button name="test3" onclick="javascript:nextLine();">Test <-</button>
+
+    <script>
+        setLine(0);
+    </script>
 </body>
--- js/player.js
+++ js/player.js
@@ -2,14 +2,70 @@
  * Created by blad3r on 08/08/2016.
  */
 
-function hello(){
-    alert("hello");
-}
+// Lyrics will be loaded here
+var lyrics = ["test1", "test2", "test3", "test4", "test5", "test6", "test7", "etc"];
+// Line where the lyric is (by time of the player)
+var actualLine = 0;
+// Where I store the paragraph items
+var catLineParagraphs = 0;
+// Timer I use to check the lyrics
 
-function getSecondPlayer(){
+function getSecondPlayer() {
     var player = document.getElementById("audioPlayer");
     var currentTime = player.currentTime;
-    console.log(currentTime);
-    alert(currentTime);
     return currentTime;
+}
+
+function getElements() {
+    if (catLineParagraphs == 0) {
+        catLineParagraphs = [
+            document.getElementById("cat_line_1"),
+            document.getElementById("cat_line_2"),
+            document.getElementById("cat_line_3"),
+            document.getElementById("cat_line_4"),
+            document.getElementById("cat_line_5")
+        ];
+    }
+    console.log(catLineParagraphs);
+}
+
+function nextLine() {
+    actualLine++;
+    setLine(actualLine);
+}
+
+function previousLine() {
+    actualLine--;
+    setLine(actualLine);
+}
+
+function setLine(lineNumber) {
+
+    if(lineNumber == undefined){
+        lineNumber = getLyricLine();//actualLine;
+    }
+    console.log("inside setLine "+ lineNumber);
+    getElements();
+    for (var i = 0; i <= 4; i++) {
+        var lyricLineNumber = lineNumber - 2 + i;
+        //if(lyricLineNumber >= 0) {
+            if(lyrics[lyricLineNumber] != undefined) {
+                catLineParagraphs[i].innerHTML = lyrics[lyricLineNumber];
+            }else{
+                catLineParagraphs[i].innerHTML = "";
+            }
+        //}
+    }
+    setTimeout(function() {
+        setLine();       // repeat
+    }, 300)
+}
+
+function getLyricLine(timeOnPlayer) {
+    if(timeOnPlayer == undefined){
+        timeOnPlayer = getSecondPlayer();
+    }
+    console.log(timeOnPlayer);
+    return(parseInt(timeOnPlayer));
+    // TODO create a method to get the line using the time of the player
 }
